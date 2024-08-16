66457ddae9018ad56ff9d3ad77eba4918e9893c9
ssergiocr01 <ssergio0@hotmail.com>
2024-08-16 13:53:41 -0600

    Creando el backend

---
+++ QuinelaAppBackend/Api/Controllers/UsuariosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+
+    }
+}
---
+++ QuinelaAppBackend/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
