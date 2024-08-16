8e54d488e4723b45ecae23c65325035b6e9d1068
ssergiocr01 <ssergio0@hotmail.com>
2024-08-16 14:19:56 -0600

    Conversión en Asyncrono

--- QuinelaAppBackend/Api/Controllers/UsuariosController.cs
+++ QuinelaAppBackend/Api/Controllers/UsuariosController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
 
 namespace Api.Controllers
 {
@@ -7,6 +10,25 @@ namespace Api.Controllers
     [ApiController]
     public class UsuariosController : ControllerBase
     {
+        private readonly ApplicationDbContext _db;
 
+        public UsuariosController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
+        {
+            var usuarios = await _db.Usuarios.ToListAsync();
+            return Ok(usuarios);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(int id)
+        {
+            var usuario = await _db.Usuarios.FindAsync(id);
+            return Ok(usuario);
+        }
     }
 }
